74f13f1a26fdddafe81d872f79a6bc99d1832295
bizelite <bizelite@naver.com>
2018-01-17 19:34:11 +0900

    Create collatz_between.py

---
+++ collatz_between.py
@@ -0,0 +1,15 @@
+def collatz_between(first, last):
+    max_cycle = 0
+    for i in range(first, last+1):
+        cycle = 1
+        while i != 1:
+            if i%2 == 0:
+                i = i / 2
+                cycle += 1
+            else:
+                i = i*3 + 1
+                cycle += 1
+        if cycle > max_cycle:
+            max_cycle, cycle = cycle, max_cycle
+    return first, last, max_cycle
+    
